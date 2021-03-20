4e725782444d6c253a526217b5a8caf581052c3a
nqvst <nqvst@users.noreply.github.com>
2021-03-21 00:45:46 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css'
 import Card from './Card'
 import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 import styled from 'styled-components'
 
 const Main = styled.div`
   max-width: 600px;
+`
+
+const Hero = styled.div`
+  & h1 {
+    font-size: 45px;
+    margin-bottom: 10px;
+  }
+  /* border-bottom: 2px solid black; */
+  margin-bottom: 20px;
+`
+
+const Socials = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  & a {
+    margin: 10px;
+    font-size: 28px;
+    padding: 0;
+    color: #111;
+  }
 `
 
 function App() {
@@ -21,6 +46,19 @@ function App() {
 
   return (
     <div className="App">
+      <Hero>
+        <h1>Jack Enqvist</h1>
+        freelance web developer
+        <Socials>
+          <a href={'https://github.com/nqvst'}>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href={'https://se.linkedin.com/in/nqvst'}>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </Socials>
+      </Hero>
+
       <Main>
         {projects.map((p) => {
           return <Card key={p.title} {...p}></Card>
